f0f97f2f0b9099caa6a3e655befd2ad74c6e363f
B0GDAN28 <125356773+B0GDAN28@users.noreply.github.com>
2023-06-29 16:24:33 +0300

    Intro

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import csv
 
 movie_url = "https://www.imdb.com/chart/top/"
 
@@ -12,6 +11,7 @@ def get_top_250(movie_url):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/114.0.0.0 Safari/537.36",
         "Accept-Encoding": "gzip, deflate, br",
+
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
                   "application/signed-exchange;v=b3;q=0.7",
         "DNT": "1",
@@ -120,11 +120,15 @@ def scrape_imdb_250(doc):
 
 Top_250_IMDB = pd.DataFrame(scrape_imdb_250(doc))
 Top_250_IMDB.to_csv("Top_Movies_250_IMDB", index=None)
-movie_urls = get_movie_links(doc)[158]
+# URL of the webscraping movie
+movie_link = get_movie_links(doc)
 
 
-def get_movie_url(movie_urls):
-    movie_doc1 = requests.get(movie_urls)
+# print(movie_url)
+
+
+def get_movie_url(movie_link):
+    movie_doc1 = requests.get(movie_link)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0",
@@ -133,79 +137,70 @@ def get_movie_url(movie_urls):
                   "application/signed-exchange;v=b3;q=0.7",
         "Upgrade-Insecure-Requests": "1"
     }
-    movie_doc2 = requests.get(movie_urls, headers=headers)
+    movie_doc2 = requests.get(movie_link, headers=headers)
     movie_doc = BeautifulSoup(movie_doc2.text, 'html.parser')
     return movie_doc
 
 
-movie_url = get_movie_url(movie_urls)
+movie_doc = get_movie_url(movie_link[0])
 
 
-def get_movie_type(movie_url):
+# print(movie_doc)
+
+
+def get_movie_type(movie_doc):
     movie_types = []
     try:
-        types = movie_url.find_all("div", class_="ipc-chip-list__scroller")
+        types = movie_doc.find_all("div", class_="ipc-chip-list__scroller")
         for types in types:
             g = types.find_all("a")
             for b in g:
                 movie_types.append(b.text)
-            return movie_types
-    except (AttributeError, ValueError,IndexError):
+        return movie_types
+    except (AttributeError, ValueError, IndexError):
         movie_types = ['Not Found']
         return movie_types
 
 
-list_movie_types = get_movie_type(movie_url)
+list_movie_types = get_movie_type(movie_doc)
+
+
 print(list_movie_types)
 
 
-def get_movie_production(movie_url):
+def get_number_user_rating(movie_doc):
+    list_number_user_rating = []
     try:
-        production_parent = movie_url.find_all(
-            class_="ipc-inline-list ipc-inline-list--show-dividers sc-afe43def-4 kdXikI baseAlt", role="presentation")
-        for production in production_parent:
-            production_child = production.find("li", class_="ipc-inline-list__item").text
-        return production_child
-    except (AttributeError, ValueError,IndexError):
-        production_child = "Not Found"
-        return production_child
-
-
-list_movies_production = get_movie_production(movie_url)
-print(list_movies_production)
-
-
-def get_number_user_rating(movie_url):
-    try:
-        numbers = movie_url.find_all("div", class_="sc-bde20123-3 bjjENQ")
+        numbers = movie_doc.find_all("div", class_="sc-bde20123-3 bjjENQ")
         for number in numbers:
-            return number.text
-    except (AttributeError, ValueError,IndexError):
+            list_number_user_rating.append(number.text)
+        return list_number_user_rating
+    except (AttributeError, ValueError, IndexError):
         number = "Not found"
         return number
 
 
-number_user_rating = get_number_user_rating(movie_url)
+number_user_rating = get_number_user_rating(movie_doc)
 print(number_user_rating)
 
 
-def get_directors(movie_url):
+def get_directors(movie_doc):
     try:
-        directors = movie_url.find("div", class_="ipc-metadata-list-item__content-container")
+        directors = movie_doc.find("div", class_="ipc-metadata-list-item__content-container")
         directors1 = [director.text for director in directors]
         return directors1
-    except (TypeError, ValueError,IndexError):
+    except (TypeError, ValueError, IndexError):
         directors1 = ['Not found']
         return directors1
 
 
-list_directors = get_directors(movie_url)
+list_directors = get_directors(movie_doc)
 print(list_directors)
 
 
-def get_writers(movie_url):
+def get_writers(movie_doc):
     try:
-        writers = movie_url.find("li", class_="ipc-metadata-list__item ipc-metadata-list-item--link")
+        writers = movie_doc.find("li", class_="ipc-metadata-list__item ipc-metadata-list-item--link")
         children = writers.findChildren("div", class_="ipc-metadata-list-item__content-container")
         writers1 = [writer.text for writer in children]
         return writers1
@@ -214,7 +209,7 @@ def get_writers(movie_url):
         return writers1
 
 
-list_writers = get_writers(movie_url)
+list_writers = get_writers(movie_doc)
 for i in range(len(list_writers)):
     spaced_name = ''
     for j in range(len(list_writers[i])):
@@ -223,35 +218,44 @@ for i in range(len(list_writers)):
         else:
             spaced_name += list_writers[i][j]
     list_writers[i] = spaced_name
-print(len(list_movies_links),len(list_movie_names),len(list_year_production),len(list_rating_stars),len(ranks),len(list_movie_types),len(list_movies_production),
-      len(number_user_rating),len(list_directors), len(list_writers))
 print(list_writers)
+print(len(list_movies_links), len(list_movie_names), len(list_year_production), len(list_rating_stars), len(ranks),
+      len(list_movie_types),
+      len(number_user_rating), len(list_directors), len(list_writers))
+
 
 top_movie_details_dict = {
     "Rank": create_rank(),
     "Movie Name": get_movie_names(doc),
-    # "Production Year": [],
+    "Production Year": get_movie_production_year(doc),
     "Rating": get_rating_stars(doc),
-    # "Genre": [],
-    # "Number of user ratings": [],
-    # "Director/Directos": [],
-    # "Writer/Writers": [],
+    "Genre": [],
+    "Number of user ratings": [],
+    "Director/Directos": [],
+    "Writer/Writers": [],
     "Link": get_movie_links(doc)
 }
-# try:
-#     for i in range(len(movie_urls)):
-#         # top_movie_details_dict["Production Year"].append(get_movie_production(movie_urls[i]))
-#         # top_movie_details_dict["Genre"].append(get_movie_type(movie_urls[i]))
-#         # top_movie_details_dict["Number of user ratings"].append(get_number_user_rating(movie_urls[i]))
-#         # top_movie_details_dict["Director/Directos"].append(get_directors(movie_urls[i]))
-#         # top_movie_details_dict["Writer/Writers"].append(get_writers(movie_urls[i]))
-#
-# except ValueError:
-#     print("DONT WORK")
+try:
+    for i in range(len(movie_link)):
+        top_movie_details_dict["Genre"].append(get_movie_type(movie_link[i]))
+        top_movie_details_dict["Number of user ratings"].append(get_number_user_rating(movie_link[i]))
+        top_movie_details_dict["Director/Directos"].append(get_directors(movie_link[i]))
+        top_movie_details_dict["Writer/Writers"].append(get_writers(movie_link[i]))
+
+except ValueError:
+    print("DONT WORK")
+print(top_movie_details_dict)
+key_lengths = {key: len(key) for key in top_movie_details_dict.keys()}
+
+# Print the lengths of each key
+for key, length in key_lengths.items():
+    print(f"Length of key '{key}': {length}")
+array_lengths = set(len(array) for array in top_movie_details_dict.values())
+if len(array_lengths) == 1:
+    print("All arrays have the same length.")
+else:
+    print("Arrays have different lengths:", array_lengths)
 
 description = pd.DataFrame(top_movie_details_dict)
 
 description.to_csv("Top_250_IMDB_MOVIES", index=False)
-
-
-
